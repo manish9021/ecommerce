f00e0b85f1e1afd3595a8e28381b830f93fb6606
= <=>
2023-07-15 15:19:04 +0530

    folder structure

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+
+import { Home, Product, Products, AboutPage, ContactPage, Cart, Login, Register, Checkout, PageNotFound } from "./pages"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+  </>
+);
